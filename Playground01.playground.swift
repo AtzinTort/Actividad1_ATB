883b7becbc5a5503c9a9eea0da33a148e9c68e6c
Atzin Tort Bravo <78048075+AtzinTort@users.noreply.github.com>
2021-08-18 12:37:04 -0500

    ACTIVIDAD 1

---
+++ Playground01.playground.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+var texto = "Esto es Swift :)"
+let numero = 4
+var cadena:String = String()
+
+cadena = "Hola Mundo. "+texto
+
+print("El contenido es: ", cadena)
+
+print("El varor de la constante es: ", numero)
